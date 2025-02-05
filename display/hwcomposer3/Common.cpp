f464951c424fc74efa183ac66dfc377cf1dfa046
Jiri Rezler <jiri.rezler@nxp.com>
2025-02-05 07:32:14 +0100

    AAUTO-1650 Android automotive detection fixed, Android automotive is using internal display types only.
    Change-Id: Idad1db414348e8b0cfffb99601333d50ed21139c
    Reviewed-on: http://androidsource.nxp.com/project/27439
    Reviewed-by: Leopold Nemcek <leopold.nemcek@nxp.com>

--- display/hwcomposer3/Common.cpp
+++ display/hwcomposer3/Common.cpp
@@ -27,7 +27,8 @@ bool IsAutoDevice() {
     // gcar_emu_x86_64, sdk_car_md_x86_64, cf_x86_64_auto, cf_x86_64_only_auto_md
     const std::string product_name = ::android::base::GetProperty("ro.product.name", "");
     return (product_name.find("car_") != std::string::npos) ||
-            (product_name.find("_auto") != std::string::npos);
+            (product_name.find("_auto") != std::string::npos) ||
+            (product_name.find("_car") != std::string::npos);
 }
 
 bool IsOverlayUserDisabled() {
