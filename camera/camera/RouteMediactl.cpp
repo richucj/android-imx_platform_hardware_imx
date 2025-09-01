52f2165bf1365b21098de9166422f619a3a30638
Frantisek Prochaska <frantisek.prochaska@nxp.com>
2025-09-01 15:11:47 +0200

    AAUTO-2190: Set ISI pad[1] resolution to 1280x800.
    Change-Id: I331fb28a056083abbac340e46462644c9769891d
    Reviewed-on: http://androidsource.nxp.com/project/29662
    Reviewed-by: Jiri Rezler <jiri.rezler@nxp.com>

--- camera/camera/RouteMediactl.cpp
+++ camera/camera/RouteMediactl.cpp
@@ -324,7 +324,7 @@ std::vector<MediaCfg> cfgGroups[1] = {
             {},
             { // pad, stream, color,    resolution, field
                 {0, 0,  UYVY8_1X16,     1280, 800,  V4L2_FIELD_NONE},
-                {1, 0,  ISI_COLORSPACE,    1280, 720,  V4L2_FIELD_NONE} // EVS need RGB888_1X24 and camera UYVY8_1X16.
+                {1, 0,  ISI_COLORSPACE,    1280, 800,  V4L2_FIELD_NONE} // EVS need RGB888_1X24 and camera UYVY8_1X16.
             },
             { // crop
                 {1, 0,  0, 40, 1280, 720}
@@ -338,7 +338,7 @@ std::vector<MediaCfg> cfgGroups[1] = {
             {},
             { // pad, stream, color,    resolution, field
                 {0, 0,  UYVY8_1X16,     1280, 800,  V4L2_FIELD_NONE},
-                {1, 0,  ISI_COLORSPACE,    1280, 720,  V4L2_FIELD_NONE}
+                {1, 0,  ISI_COLORSPACE,    1280, 800,  V4L2_FIELD_NONE}
             },
             { // crop
                 {1, 0,  0, 40, 1280, 720}
@@ -352,7 +352,7 @@ std::vector<MediaCfg> cfgGroups[1] = {
             {},
             { // pad, stream, color,    resolution, field
                 {0, 0,  UYVY8_1X16,     1280, 800,  V4L2_FIELD_NONE},
-                {1, 0,  ISI_COLORSPACE,    1280, 720,  V4L2_FIELD_NONE}
+                {1, 0,  ISI_COLORSPACE,    1280, 800,  V4L2_FIELD_NONE}
             },
             { // crop
                 {1, 0,  0, 40, 1280, 720}
@@ -366,7 +366,7 @@ std::vector<MediaCfg> cfgGroups[1] = {
             {},
             { // pad, stream, color,    resolution, field
                 {0, 0,  UYVY8_1X16,     1280, 800,  V4L2_FIELD_NONE},
-                {1, 0,  ISI_COLORSPACE,    1280, 720,  V4L2_FIELD_NONE}
+                {1, 0,  ISI_COLORSPACE,    1280, 800,  V4L2_FIELD_NONE}
             },
             { // crop
                 {1, 0,  0, 40, 1280, 720}
